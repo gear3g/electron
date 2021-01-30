9107f72efb95e443722078f55db7c7c04823e137
gustavogra <granados.gg@gmail.com>
2021-01-30 08:13:55 -0600

    Added Hot Reload for dev env

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow } from 'electron';
+import isDev from 'electron-is-dev';
 
 const createWindow = (): void => {
     let win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            nodeIntegration: true
-        }
+            nodeIntegration: true,
+        },
     });
 
-    win.loadFile('index.html');
+    console.log(isDev);
+    win.loadURL(
+        isDev
+            ? 'http://localhost:9000'
+            : `file:///${app.getAppPath()}/index.html`
+    );
 };
 
-app.on("ready", createWindow);
+app.on('ready', createWindow);
